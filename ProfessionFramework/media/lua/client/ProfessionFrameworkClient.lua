24370c36c42bf1c45209de6d0d9ffc45682eec29
DrunkDwarf <DrunkDwarf@users.noreply.github.com>
2018-07-08 13:43:08 +0100

    made profession spawn regions code simpler

--- ProfessionFramework/media/lua/client/ProfessionFrameworkClient.lua
+++ ProfessionFramework/media/lua/client/ProfessionFrameworkClient.lua
@@ -53,18 +53,9 @@ Events.OnSpawnRegionsLoaded.Add(function(regions)
     for profession, details in pairs(ProfessionFramework.Professions) do
         if details.spawns then
             for i = 1, #regions do
-
-                ProfessionFramework.log(ProfessionFramework.INFO, "Injecting Custom Spawn Regions for " .. profession .. " in " .. regions[i].name)
-
-                -- Inject the Custom Spawn Locations for this region
-                if regions[i].name == ProfessionFramework.regions['muldraugh'] and details.spawns['muldraugh'] then
-                    regions[i].points[profession] = details.spawns['muldraugh']
-                elseif regions[i].name == ProfessionFramework.regions['westpoint'] and details.spawns['westpoint'] then
-                    regions[i].points[profession] = details.spawns['westpoint']
-                elseif regions[i].name == ProfessionFramework.regions['rosewood'] and details.spawns['rosewood'] then
-                    regions[i].points[profession] = details.spawns['rosewood']
-                elseif regions[i].name == ProfessionFramework.regions['riverside'] and details.spawns['riverside'] then
-                    regions[i].points[profession] = details.spawns['riverside']
+                if details.spawns[_regions[i].name] then
+                    ProfessionFramework.log(ProfessionFramework.INFO, "Injecting Custom Spawn Regions for " .. profession .. " in " .. regions[i].name)
+                    _regions[i].points[profession] = details.spawns[_regions[i].name];
                 end
             end
         end
--- ProfessionFramework/media/lua/shared/2ProfessionFramework.lua
+++ ProfessionFramework/media/lua/shared/2ProfessionFramework.lua
@@ -8,14 +8,6 @@ ProfessionFramework = {
     RemoveDefaultTraits = false,
     AlwaysUseStartingKits = true,
     LogLevel = 2,
-
-    -- Spawn Regions
-    regions = {
-        muldraugh = "Muldraugh, KY",
-        westpoint = "West Point, KY",
-        rosewood = "Rosewood, KY",
-        riverside = "Riverside, KY",
-    },
         
     -- logging constants
     ERROR = 0,
